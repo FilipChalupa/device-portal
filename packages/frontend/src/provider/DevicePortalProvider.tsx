93fa0d0f1e9dd7457f8faa9951478e1aa43551cd
Filip Chalupa <chalupa.filip@gmail.com>
2026-02-20 16:44:18 +0100

    refactor: reorganize imports and enhance DevicePortalProvider structure

--- packages/frontend/src/provider/DevicePortalProvider.tsx
+++ packages/frontend/src/provider/DevicePortalProvider.tsx
@@ -1,14 +1,37 @@
-import { type FunctionComponent } from 'react'
+import {
+	createContext,
+	useContext,
+	type FunctionComponent,
+	type ReactNode,
+} from 'react'
 import {
 	useDevicePortalProvider,
 	type DevicePortalProviderOptions,
 } from './useDevicePortalProvider'
+import { type Initiator } from '../webrtc/Initiator'
+
+type DevicePortalContextValue = {
+	activePeers: string[]
+	initiator: Initiator | null
+}
+
+const DevicePortalContext = createContext<DevicePortalContextValue>({
+	activePeers: [],
+	initiator: null,
+})
+
+export const useDevicePortal = () => useContext(DevicePortalContext)
 
 export const DevicePortalProvider: FunctionComponent<{
 	room: string
 	data: string
 	options?: DevicePortalProviderOptions
-}> = ({ room, data, options }) => {
-	useDevicePortalProvider(room, data, options)
-	return null
+	children?: ReactNode
+}> = ({ room, data, options, children }) => {
+	const value = useDevicePortalProvider(room, data, options)
+	return (
+		<DevicePortalContext.Provider value={value}>
+			{children}
+		</DevicePortalContext.Provider>
+	)
 }
