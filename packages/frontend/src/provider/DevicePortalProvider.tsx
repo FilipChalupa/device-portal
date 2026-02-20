f8adf21780854ecbee7390444df32b5dc44e5f0e
Filip Chalupa <chalupa.filip@gmail.com>
2026-02-20 17:01:27 +0100

    refactor: remove unused context and simplify DevicePortalProvider structure

--- packages/frontend/src/provider/DevicePortalProvider.tsx
+++ packages/frontend/src/provider/DevicePortalProvider.tsx
@@ -1,38 +1,14 @@
-import {
-	createContext,
-	useContext,
-	type FunctionComponent,
-	type PropsWithChildren,
-} from 'react'
-import { type Initiator } from '../webrtc/Initiator'
+import { type FunctionComponent } from 'react'
 import {
 	useDevicePortalProvider,
 	type DevicePortalProviderOptions,
 } from './useDevicePortalProvider'
 
-type DevicePortalContextValue = {
-	peers: string[]
-	initiator: Initiator | null
-}
-
-const DevicePortalContext = createContext<DevicePortalContextValue>({
-	peers: [],
-	initiator: null,
-})
-
-export const useDevicePortal = () => useContext(DevicePortalContext)
-
-export const DevicePortalProvider: FunctionComponent<
-	PropsWithChildren<{
-		room: string
-		data: string
-		options?: DevicePortalProviderOptions
-	}>
-> = ({ room, data, options, children }) => {
-	const value = useDevicePortalProvider(room, data, options)
-	return (
-		<DevicePortalContext.Provider value={value}>
-			{children}
-		</DevicePortalContext.Provider>
-	)
+export const DevicePortalProvider: FunctionComponent<{
+	room: string
+	data: string
+	options?: DevicePortalProviderOptions
+}> = ({ room, data, options }) => {
+	useDevicePortalProvider(room, data, options)
+	return null
 }
