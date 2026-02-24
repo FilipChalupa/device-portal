fd36a7a0a6a8059fe3013036a83f1457ae49754e
Filip Chalupa <chalupa.filip@gmail.com>
2026-02-24 15:53:07 +0100

    feat: add build argument for websocket signaling server in Dockerfile

--- packages/react/src/stories/utilities/websocketSignalingServer.ts
+++ packages/react/src/stories/utilities/websocketSignalingServer.ts
@@ -1,3 +1,5 @@
-export const websocketSignalingServer = import.meta.env.DEV
-	? 'ws://localhost:8080'
-	: 'wss://device-portal.filipchalupa.cz'
+export const websocketSignalingServer =
+	import.meta.env.VITE_WEBSOCKET_SIGNALING_SERVER ??
+	(import.meta.env.DEV
+		? 'ws://localhost:8080'
+		: 'wss://device-portal.filipchalupa.cz')
