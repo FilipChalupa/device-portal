b35fd27e693a1a125d3c533138b90127408e753f
Filip Chalupa <chalupa.filip@gmail.com>
2026-02-25 13:09:40 +0100

    fix: remove @storybook/addon-onboarding from package dependencies

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
 
 	addons: [
 		getAbsolutePath('@storybook/addon-links'),
-		getAbsolutePath('@storybook/addon-onboarding'),
 		getAbsolutePath('@storybook/addon-docs'),
 	],
 
